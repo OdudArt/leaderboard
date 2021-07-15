6d023b2e8425b93d458673443c1ddd6c6e09178f
Odud Artyom <odudartyom@gmail.com>
2021-07-15 18:18:25 +0300

    resolved

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { mergeMap, map, reduce, concatMap } from 'rxjs/operators';
+import {from} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,33 @@ import { Component } from '@angular/core';
     </h2>
     {{ ip$ | async }}
     <h2>Leaderboard</h2>
-    TODO - Your stuff should go in here...`,
+    TODO - Your stuff should go in here...
+    <ul>
+      <li *ngFor="let player of leaders$ | async">
+        {{player}}
+      </li>
+    </ul>
+    `,
 })
 export class AppComponent {
   ip$ = this.http.get('/ipify', { responseType: 'text' });
-
+  
+  leaders$ = this.http.post('http://localhost:4000/api/rpc', JSON.stringify({
+    "jsonrpc": "2.0",
+    "method": "getLeaderboard",
+    "params": [],
+    "id": 12345
+  })).pipe(
+    map((param: any) => param.result),
+    concatMap((params) => from(params)),
+    mergeMap((param) => this.http.post('http://localhost:4000/api/rpc', JSON.stringify({
+      "jsonrpc": "2.0",
+      "method": "getPlayer",
+      "params": [param],
+      "id": 12345
+    }))),
+    reduce((acc, i: any) => [...acc, i.result.name], [] as string[])
+  )
+  
   constructor(private http: HttpClient) {}
 }
